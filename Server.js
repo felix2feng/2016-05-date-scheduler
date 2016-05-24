6c09274c232518d57ca5fbc0747677d95fdb9980
Felix <felix2feng@gmail.com>
2016-05-24 01:32:59 -0700

    Connected mogodb

--- Server.js
+++ Server.js
@@ -8,14 +8,18 @@ var bodyParser = require('body-parser')
 var googleAuth = require('google-auth-library');
 var bluebird = require('bluebird');
 var morgan = require('morgan');
+var mongodb = require('mongodb');
+var mongoose = require('mongoose');
+var db = require('./db.js');
 
 // Google configurations
 var publicAsset = __dirname + '/public';
 
-app.use(express.static(publicAsset));
-app.use(morgan('combined')); // log requests to the console output
+// app.use(express.static(publicAsset));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev')); // log requests to the console output
 // app.use(favicon(__dirname + '/public/favicon.ico')); // serve the tab icon the browser display for your page
-// app.use(express.static('public')); // serve public/</i> as a static resource (just returns the file, images, scripts)
+app.use(express.static('public')); // serve public/</i> as a static resource (just returns the file, images, scripts)
 // app.use(methodOverride()); // simulate DELETE and PUT calls if the client is using X-HTTP-Method-Override
 // app.use(cookieParser()); // parse <code>req.headers.cookie</code> into <code>req.cookies</code>
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded request into req.body
@@ -23,13 +27,28 @@ app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-
 
 
 app.get('/', function(req, res) {
-	res.sendFile(publicAsset + '/calendate.html')	
+	res.sendFile(publicAsset + '/calendate.html');	
+  // res.send('hello world');
 });
 
 app.post('/', function(req, res) {
-	// Add that data in a temporary storage
-	// Send back a success response
-  console.log('This is the request: ', req.body);
+  var meeting = new db.Meeting({
+    name: req.body.name,
+    phoneNumber: req.body.phone0 + '-' + req.body.phone1 + '-' + req.body.phone2,
+    location: req.body.location, // Need something
+    date: req.body.date, // Need something
+    time: req.body.time, // Need something
+    shortDescription: req.body.shortDescription,
+    description: req.body.description
+  })
+
+  meeting.save(function(err, meeting) {
+    if (err) {
+      console.log(err);
+    } 
+    console.log('saved successfully', meeting);
+  })
+  console.log('Made it here');
 	res.status(200).end();
 });
 
